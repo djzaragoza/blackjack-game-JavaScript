3941434b3272534809a61098c4d0d49202dd9b1f
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-08 11:57:33 -0700

    building functionality all in JS

---
+++ index.js
@@ -0,0 +1,43 @@
+
+"use strict";
+
+var numCardsPulled = 9;
+
+var player = {
+    cards: [],
+    score: 0,
+    money: 100
+};
+
+var dealer = {
+    cards: [],
+    score: 0
+};
+
+document.getElemebtById("player-money").innerHTML = "Your money: $" + player.money;
+document.getElementById("hit-button").disabled = true;
+document.getElementById("stand-button").disabled = true;
+
+function getCardsValue(a) {
+    var cardArray = [],
+        sum = 0,
+        i = 0,
+        aceCount - 0;
+    cardArray = a;
+    for (i; i < cardArray.length; i += i) {
+        if (cardArray[i].rank === "J" || cardArray[i].rank === "Q" || cardArray[i].rank === "K"){
+            sum =+ 10;
+        } else if (cardArray[i].rank === "A") {
+            sum += 11;
+            aceCount += 1;
+        } else {
+            sum += cardArray[i].rank;
+        }
+    }
+    while (aceCount > 0 && sum > 21) {
+        sum -= 10;
+        aceCount -= 1;
+    }
+    return sum;
+}
+
