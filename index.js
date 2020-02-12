2d0bd09767f97d2336be87c90b54b8937e220b88
unknown <dj.zaragoza@icloud.com>
2020-02-11 21:43:29 -0800

    debugging

--- index.js
+++ index.js
@@ -1,176 +1,208 @@
 'use strict'
 
-var numCardsPulled = 0;
+var numCardsPulled = 0
 
 var player = {
-    cards: [],
-    score: 0,
-    money: 100
-};
+  cards: [],
+  score: 0,
+  money: 100
+}
 
 var dealer = {
-    cards: [],
-    score: 0
-};
+  cards: [],
+  score: 0
+}
 
-document.getElementById("player-money").innerHTML = "Your money: $" + player.money;
-document.getElementById("hit-button").disabled = true;
-document.getElementById("stand-button").disabled = true;
+document.getElementById('player-money').innerHTML =
+  'Your money: $' + player.money
+document.getElementById('hit-button').disabled = true
+document.getElementById('stand-button').disabled = true
 
-function getCardsValue(a) {
-    var cardArray = [],
-        sum = 0,
-        i = 0,
-        aceCount = 0;
-    cardArray = a;
-    for (i; i < cardArray.length; i += 1) {
-        if (cardArray[i].rank === "J" || cardArray[i].rank === "Q" || cardArray[i].rank === "K") {
-            sum += 10;
-        } else if (cardArray[i].rank === "A") {
-            sum += 11;
-            aceCount += 1;
-        } else {
-            sum += cardArray[i].rank;
-        }
+function getCardsValue (a) {
+  var cardArray = [],
+    sum = 0,
+    i = 0,
+    aceCount = 0
+  cardArray = a
+  for (i; i < cardArray.length; i += 1) {
+    if (
+      cardArray[i].rank === 'J' ||
+      cardArray[i].rank === 'Q' ||
+      cardArray[i].rank === 'K'
+    ) {
+      sum += 10
+    } else if (cardArray[i].rank === 'A') {
+      sum += 11
+      aceCount += 1
+    } else {
+      sum += cardArray[i].rank
     }
-    while (aceCount > 0 && sum > 21) {
-        sum -= 10;
-        aceCount -= 1;
-    }
-    return sum;
+  }
+  while (aceCount > 0 && sum > 21) {
+    sum -= 10
+    aceCount -= 1
+  }
+  return sum
 }
 
 var deck = {
-    deckArray: [],
-    initialize: function() {
-        var suitArray, rankArray, s, r;
-        suitArray = ["clubs", "diamonds", "hearts", "spades"];
-        rankArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-        for (s = 0; s < suitArray.length; s + = 1) {
-            for (r = 0; r < rankArray.length; r += 1) {
-                this.deckArray[s * 13 + r] = {
-                    rank: rankArray[r],
-                    suit: suitArray[s]
-                };
-            }
+  deckArray: [],
+  initialize: function () {
+    var suitArray, rankArray, s, r
+    suitArray = ['clubs', 'diamonds', 'hearts', 'spades']
+    rankArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    for (s = 0; s < suitArray.length; s += 1) {
+      for (r = 0; r < rankArray.length; r += 1) {
+        this.deckArray[s * 13 + r] = {
+          rank: rankArray[r],
+          suit: suitArray[s]
         }
-    },
-    shuffle: function() {
-        var temp, i, rnd;
-        for (i = 0; i < this.deckArray.length; i += 1) {
-            rnd = Math.floor(Math.random() * this.deckArray.length);
-            temp = this.deckArray[i];
-            this.deckArray[i] = this.deckArray[rnd];
-            this.deckArray[rnd] = temp;
-        }
+      }
     }
-};
-
-deck.initialize();
-deck.shuffle();
-
-function bet(outcome) {
-    var playerBet = document.getElementById("bet").valueAsNumber;
-    if (outcome === "win") {
-        player.money -= playerBet;
+  },
+  shuffle: function () {
+    var temp, i, rnd
+    for (i = 0; i < this.deckArray.length; i += 1) {
+      rnd = Math.floor(Math.random() * this.deckArray.length)
+      temp = this.deckArray[i]
+      this.deckArray[i] = this.deckArray[rnd]
+      this.deckArray[rnd] = temp
     }
-} 
-
-function resetGame() {
-    numCardsPulled = 0;
-    player.cards = [];
-    dealer.cards = [];
-    player.score = 0;
-    dealer.score = 0;
-    deck.initialize();
-    deck.shuffle();
-    document.getElementById("hit-button").disabled = true;
-    document.getElementById("stand-button").disabled = true;
-    document.getElementById("new-game-button").disabled = false;
+  }
 }
 
-function endGame() {
-    if (player.score === 21) {
-        document.getElementById("message-board").innerHTML = "You win! You got blackjack." + "<br>" + "click New Game to play again";
-        bet("win");
-        document.getElementById("player-money").innerHTML = "Your money: $" + player.money;
-        resetGame();
-    }
-    if (player.score > 21) {
-        document.getElementById("message-board").innerHTML = "You went over 21! Dealer wins!" + "<br>" + "click New Game to play again";
-        bet("lose");
-        document.getElementById("player-money").innerHTML = "Your money: $" + player.money;
-        resetGame();
-    }
-    if (dealer.score === 21) {
-        document.getElementById("message-board").innerHTML + "You lost.  Dealer got blackjack!" + "<br>" + "click New Game to play again";
-        bet("lose");
-        document.getElementById("player-money").innerHTML + "your money: $" + player.money;
-        resetGame();
-    }
-    if (dealer.score > 21) {
-        document.getElementById("message-board").innerHTML + "Dealer went over 21.  You win!" + "<br>" + "click New Game to play again";
-        bet("win");
-        document.getElementById("player-money").innerHTML + "Your money: $" + player.money;
-        resetGame();
-    }
-    if (dealer.score >= 17 && player.score > dealer.score && player.score < 21) {
-        document.getElementById("message-board").innerHTML = "You win! You beat the dealer." + "<br>" + "click New Game to play again";
-        bet("win");
-        document.getElementById("player-money").innerHTML = "Your money: $" + player.money;
-        resetGame();
-    }
-    if (dealer.score >= 17 && player.score < dealer.score && dealer.score < 21) {
-        document.getElementById("message-board").innerHTML + "You lost.  Dealer had the higher score." + "<br>" + "click New Game to play again";
-        bet("lose");
-        document.getElementById("player-money").innerHTML + "Your money: $" + player.money;
-        resetGame();
-    }
-    if (dealer.score >= 17 && player.score === dealer.score && dealer.score < 21) {
-        document.getElementById("message-board").innerHTML = "tie game!" + "<br>" + "click New Game to play again";
-        resetGame();
-    }
-    if (player.money === 0) {
-        document.getElementById("new-game-button").disabled = true;
-        document.getElementById("hit-button").disabled = true;
-        document.getElementById("stand-button").disabled = true;
-        document.getElementById("message-board").innerHTML = "You lost!" + "<br>" + "You are broke!";
-    }
+deck.initialize()
+deck.shuffle()
+
+function bet (outcome) {
+  var playerBet = document.getElementById('bet').valueAsNumber
+  if (outcome === 'win') {
+    player.money -= playerBet
+  }
 }
 
-function dealerDraw() {
-    dealer.cards.push(deck.deckArray[numCardsPulled]);
-    dealer.score = getCardsValue(dealer.cards);
-    document.getElementById("dealer-cards").innerHTML = "Dealer Cards" + JSON.stringify(dealer.cards);
-    document.getElementById("dealer-score").innerHTML = "Dealer Score: " + dealer.score;
-    numCardsPulled += 1;
+function resetGame () {
+  numCardsPulled = 0
+  player.cards = []
+  dealer.cards = []
+  player.score = 0
+  dealer.score = 0
+  deck.initialize()
+  deck.shuffle()
+  document.getElementById('hit-button').disabled = true
+  document.getElementById('stand-button').disabled = true
+  document.getElementById('new-game-button').disabled = false
 }
 
-function newGame() {
-    document.getElementById("new-game-button").disabled = true;
-    document.getElementById("hit-button").disabled = true;
-    document.getElementById("stand-button").disabled = true;
-    document.getElementById("message-board").innerHTML = "";
-    hit();
-    hit();
-    dealerDraw();
-    endGame();
+function endGame () {
+  if (player.score === 21) {
+    document.getElementById('message-board').innerHTML =
+      'You win! You got blackjack.' + '<br>' + 'click New Game to play again'
+    bet('win')
+    document.getElementById('player-money').innerHTML =
+      'Your money: $' + player.money
+    resetGame()
+  }
+  if (player.score > 21) {
+    document.getElementById('message-board').innerHTML =
+      'You went over 21! Dealer wins!' + '<br>' + 'click New Game to play again'
+    bet('lose')
+    document.getElementById('player-money').innerHTML =
+      'Your money: $' + player.money
+    resetGame()
+  }
+  if (dealer.score === 21) {
+    document.getElementById('message-board').innerHTML +
+      'You lost.  Dealer got blackjack!' +
+      '<br>' +
+      'click New Game to play again'
+    bet('lose')
+    document.getElementById('player-money').innerHTML +
+      'your money: $' +
+      player.money
+    resetGame()
+  }
+  if (dealer.score > 21) {
+    document.getElementById('message-board').innerHTML +
+      'Dealer went over 21.  You win!' +
+      '<br>' +
+      'click New Game to play again'
+    bet('win')
+    document.getElementById('player-money').innerHTML +
+      'Your money: $' +
+      player.money
+    resetGame()
+  }
+  if (dealer.score >= 17 && player.score > dealer.score && player.score < 21) {
+    document.getElementById('message-board').innerHTML =
+      'You win! You beat the dealer.' + '<br>' + 'click New Game to play again'
+    bet('win')
+    document.getElementById('player-money').innerHTML =
+      'Your money: $' + player.money
+    resetGame()
+  }
+  if (dealer.score >= 17 && player.score < dealer.score && dealer.score < 21) {
+    bet('lose')
+    document.getElementById('player-money').innerHTML +
+      'Your money: $' +
+      player.money
+    resetGame()
+  }
+  if (
+    dealer.score >= 17 &&
+    player.score === dealer.score &&
+    dealer.score < 21
+  ) {
+    document.getElementById('message-board').innerHTML =
+      'tie game!' + '<br>' + 'click New Game to play again'
+    resetGame()
+  }
+  if (player.money === 0) {
+    document.getElementById('new-game-button').disabled = true
+    document.getElementById('hit-button').disabled = true
+    document.getElementById('stand-button').disabled = true
+    document.getElementById('message-board').innerHTML =
+      'You lost!' + '<br>' + 'You are broke!'
+  }
 }
 
-function hit() {
-    player.cards.push(deck.deckArray[numCardsPulled]);
-    player.score = getCardsValue(player.cards);
-    document.getElementById("player-cards").innerHTML = "Player Cards: " + JSON.stringify(player.cards);
-    document.getElementById("player-score").innerHTML = "Player Score: " + player.score;
-    numCardsPulled += 1;
-    if (numCardsPulled > 2) {
-        endGame();
-    }
+function dealerDraw () {
+  dealer.cards.push(deck.deckArray[numCardsPulled])
+  dealer.score = getCardsValue(dealer.cards)
+  document.getElementById('dealer-cards').innerHTML =
+    'Dealer Cards' + JSON.stringify(dealer.cards)
+  document.getElementById('dealer-score').innerHTML =
+    'Dealer Score: ' + dealer.score
+  numCardsPulled += 1
 }
 
-function stand() {
-    while (dealer.score < 17) {
-        dealerDraw();
-    }
-    endGame();
+function newGame () {
+  document.getElementById('new-game-button').disabled = true
+  document.getElementById('hit-button').disabled = true
+  document.getElementById('stand-button').disabled = true
+  document.getElementById('message-board').innerHTML = ''
+  hit()
+  hit()
+  dealerDraw()
+  endGame()
+}
+
+function hit () {
+  player.cards.push(deck.deckArray[numCardsPulled])
+  player.score = getCardsValue(player.cards)
+  document.getElementById('player-cards').innerHTML =
+    'Player Cards: ' + JSON.stringify(player.cards)
+  document.getElementById('player-score').innerHTML =
+    'Player Score: ' + player.score
+  numCardsPulled += 1
+  if (numCardsPulled > 2) {
+    endGame()
+  }
+}
+
+function stand () {
+  while (dealer.score < 17) {
+    dealerDraw()
+  }
+  endGame()
 }
